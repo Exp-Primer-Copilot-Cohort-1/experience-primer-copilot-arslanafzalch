1ab7bc6f514fb6ddf4ed97fa8ed48b7753b98b61
Muhammad Arslan Afzal <arslanafzalch515@gmail.com>
2023-11-12 12:18:26 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,26 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const commentsByPostId = {};
+// Get comments by post id
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+// Create comments by post id
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
+});
+// Listen port
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
